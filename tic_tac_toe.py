a326be1f6575ae53625e67f240754d95ee4fdf36
Feisal Ahmed <feisala132@gmail.com>
2023-05-17 14:14:59 +0100

    Adding user inputs

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,18 @@
 print("This is a Tic Tac Toe Game")
 print('-----------------------------')
+print()
+
+board = '''----|----|----|\n----|----|----|\n----|----|----|'''
+choices = ['X', 'O']
+p1 = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
+p2 = ''
+
+if p1.upper() == 'X':
+    p2 = p2 + choices[1]
+    print(f'Player two your choice will be {p2}')
+else:
+    p2 = p2 + choices[0]
+    print(f'Player two your choice will be {p2}')
+
 
 
