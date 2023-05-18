d3304e9371a2cf6f3dc641a3e11ea6dbd7e184ab
Feisal Ahmed <feisala132@gmail.com>
2023-05-18 19:58:50 +0100

    Adding while loop to ensure valid input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,19 +4,26 @@ print()
 
 choices = ['X', 'O']
 print()
-p1 = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
-p2 = ''
+while True:
+    p1 = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
+    p2 = ''
 
-### This will need to be refactored later ### 
-# to not allow any other input but 'X' and 'O'
-# and it also needs to be in a while loop
-# till a suitable input is provided
-if p1.upper() == 'X':
-    p2 = p2 + choices[1]
-    print(f'Player two your choice will be {p2}')
-else:
-    p2 = p2 + choices[0]
-    print(f'Player two your choice will be {p2}')
+    if p1.upper() == 'X':
+        p2 = p2 + choices[1]
+        print(f'Player two your choice will be {p2}')
+        break
+    elif p1.upper() == 'O':
+        p2 = p2 + choices[0]
+        print(f'Player two your choice will be {p2}')
+        break
+    else:
+        print('Enter a letter X or O')
+        p1 = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
+
+
+    
+
+
 
 
   
