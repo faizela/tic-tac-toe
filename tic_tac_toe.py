b1b47069d5c96c23a4d7e3cb9a8c7f6808f9af12
Feisal Ahmed <feisala132@gmail.com>
2023-05-18 17:44:46 +0100

    Changing board layout and approach to access it

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,7 @@
 print("This is a Tic Tac Toe Game")
 print('-----------------------------')
 print()
-board = '''----|----|----|\n----|----|----|\n----|----|----|'''
-print(board)
+
 choices = ['X', 'O']
 print()
 p1 = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
@@ -19,28 +18,6 @@ else:
     p2 = p2 + choices[0]
     print(f'Player two your choice will be {p2}')
 
-def rows_dict():
-    rows_dic = {}
-    count = 0
-    board_arr = board.split('\n')
-    for row in board_arr:
-        rows_dic[f'R{1 + count}'] = row
-        count += 1
-
-    return rows_dic
-
-
-# 
-
-# def row_1():
-#     r1_str = rows_list()[0]
-#     r1_dic = {'r1' : r1_str}
-#     return r1_dic
-
-# def row_1_cells():
-#     r1_dic = row_1()
-#     r1_str = r1_dic['r1']
-#     return r1_str.split('|')
 
   
 
