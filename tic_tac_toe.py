39addd614763042fbd688227ad6ba24100611e9b
Feisal Ahmed <feisala132@gmail.com>
2023-05-19 19:51:14 +0100

    Trapping bad input in try except block and printing updated game board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -34,16 +34,30 @@ def game_board():
 
 game_board()
 print() # for space
-while True:
+running = True
+while running:
     print('The cell blocks are 1 to nine')
-    player_1 = input(f'Player_1 - Where would You like to place your {p1_letter.upper()} in which cell block [1 - 9]?')
+    allowed_nums = [1,2,3,4,5,6,7,8,9]
+    player_1_str = input(f'Player_1 - Where would You like to place your {p1_letter.upper()} in which cell block [1 - 9]? ')
+    try:
+        player_1_int = int(player_1_str) 
+        for num in allowed_nums:
+            if num == player_1_int:
+                board[num - 1] = p1_letter.upper()
+                game_board()
+                running = False
+   
+    except ValueError:
+            print('Enter a valid number between 1 to 9')
 
 
-    
 
+   
+            
+        
 
+            
 
-    
 
 
 def vertical_win_x(board):
