1ad2a7f4b433e0728dcb9ee0a8d65560b7409a29
Feisal Ahmed <feisala132@gmail.com>
2023-05-21 19:42:07 +0100

    Refactoring code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,7 +4,7 @@ board = ['-','-','-',
         '-','-','-',
         '-','-','-']
 
-choices = ['X', 'O']
+choices = 
 
 while True:
     p1_letter = input("Player 1 - What letter would you like to choose 'X' or 'O' ")
@@ -20,8 +20,7 @@ while True:
         break
     else:
         print('Enter a letter X or O')
-            
-        
+   
 
 def game_board():
     print(board[0] + ' | ' + board[1] + ' | ' + board[2])
@@ -58,7 +57,15 @@ def diagnols_win_o(board):
     return board[0] == 'O' and board[4] == 'O' and board[8] == 'O' or \
     board[2] == 'O' and board[4] == 'O' and board[6] == 'O'
 
+def playerInput(board, player):
+        inp = int(input("Enter a number 1-9: "))
+        if inp >= 1 and inp <= 9 and board[inp-1] == '-':
+            board[inp-1] = player
+        else:
+            print("That position is already taken by other player")
 
+        
+print()
 game_board()
 print() # for space
 running = True
@@ -66,90 +73,114 @@ while running:
     allowed_nums = [1,2,3,4,5,6,7,8,9]
     P1_WIN_MSG = 'Player 1 Wins!'
     P2_WIN_MSG = 'Player 2 Wins!'
+    playerInput(board, p1_letter)
     count =  0 # count variablle to check for '-' used for checking for draw
-    try:
-        player_1_int = int(input(f'Player_1 - Where would You like to place your {p1_letter.upper()} in which cell block [1 - 9]? '))
-        if player_1_int not in allowed_nums:
-            print('Enter a valid number from 1 to 9')     
-        for num in allowed_nums:
-            if num == player_1_int:
-                board[num - 1] = p1_letter.upper()
-                game_board()
-                if rows_win_x(board) and p1_letter.upper() == 'X':
-                    running = False
-                    print(P1_WIN_MSG)
+      
+    # Player one
+   
 
-                if cols_win_x(board) and p1_letter.upper() == 'X':
-                    running = False
-                    print(P1_WIN_MSG)
+   
 
-                if diagnols_win_x(board) and p1_letter.upper() == 'X':
-                    running = False
-                    print(P1_WIN_MSG)
-
-                if rows_win_o(board) and p1_letter.upper() == 'O':
-                    running = False
-                    print(P1_WIN_MSG)
-
-                if cols_win_o(board) and p1_letter.upper() == 'O':
-                    running = False
-                    print(P1_WIN_MSG)
-
-                if diagnols_win_o(board) and p1_letter.upper() == 'O':
-                    running = False
-                    print(P1_WIN_MSG)
+    # # Player two 
+        # if running:
+        #     try:
+        #         player_2_int = int(input(f'Player 2 - Where would You like to place your {p2_letter.upper()} in which cell block [1 - 9]? '))
+        #         if player_2_int not in allowed_nums:
+        #                     print('Enter a valid number from 1 to 9')
+        #     except ValueError:
+        #         print('Enter a valid number between 1 to 9')
+        #     else:
+        #         try:
+        #             if player_2_int not in allowed_nums:
+        #                     print('Enter a valid number from 1 to 9')
+        #         except:
+        #             print('Enter a valid number between 1 to 9')
+        #         else:
+        #             for num in allowed_nums:
+        #                 if num == player_2_int:
+        #                     board[num - 1] = p2_letter.upper()
+                                
+        #     game_board()
+                
         
-        for i in range(len(board)):
-            if running and board[i] != '-':
-                count += 1
-            if count == 9:
-                running = False
-                print("it's a draw")
+
+
+    # game_board()
+    #             if rows_win_x(board) and p1_letter.upper() == 'X':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+
+    #             if cols_win_x(board) and p1_letter.upper() == 'X':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+
+    #             if diagnols_win_x(board) and p1_letter.upper() == 'X':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+
+    #             if rows_win_o(board) and p1_letter.upper() == 'O':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+
+    #             if cols_win_o(board) and p1_letter.upper() == 'O':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+
+    #             if diagnols_win_o(board) and p1_letter.upper() == 'O':
+    #                 running = False
+    #                 print(P1_WIN_MSG)
+        
+    #     for i in range(len(board)):
+    #         if running and board[i] != '-':
+    #             count += 1
+    #         if count == 9:
+    #             running = False
+    #             print("it's a draw")
 
                 
 
     
-        #### player two ####
-        if running:
-            player_2_int = int(input(f'Player_2 - Where would You like to place your {p2_letter.upper()} in which cell block [1 - 9]? '))
-            if player_2_int not in allowed_nums:
-                print('Enter a valid number from 1 to 9')
-            for num in allowed_nums:
-                if num == player_2_int:
-                    board[num - 1] = p2_letter.upper()
-                    game_board()
-                    if rows_win_x(board) and p2_letter.upper() == 'X':
-                        running = False
-                        print(P2_WIN_MSG)
+    #     #### player two ####
+    #     if running:
+    #         try:
+    #             player_2_int = int(input(f'Player_2 - Where would You like to place your {p2_letter.upper()} in which cell block [1 - 9]? '))
+    #             if player_2_int not in allowed_nums:
+    #                 print('Enter a valid number from 1 to 9')
+    #         except ValueError:
+    #             print('Enter a valid number between 1 to 9')
+    #         for num in allowed_nums:
+    #             if num == player_2_int:
+    #                 board[num - 1] = p2_letter.upper()
+    #                 game_board()
+    #                 if rows_win_x(board) and p2_letter.upper() == 'X':
+    #                     running = False
+    #                     print(P2_WIN_MSG)
 
-                    if cols_win_x(board) and p2_letter.upper() == 'X':
-                        running = False
-                        print(P2_WIN_MSG)
+    #                 if cols_win_x(board) and p2_letter.upper() == 'X':
+    #                     running = False
+    #                     print(P2_WIN_MSG)
 
-                    if diagnols_win_x(board) and p2_letter.upper() == 'X':
-                        running = False
-                        print(P2_WIN_MSG)
+    #                 if diagnols_win_x(board) and p2_letter.upper() == 'X':
+    #                     running = False
+    #                     print(P2_WIN_MSG)
 
-                    if rows_win_o(board) and p2_letter.upper() == '0':
-                        running = False
-                        print(P2_WIN_MSG)
+    #                 if rows_win_o(board) and p2_letter.upper() == '0':
+    #                     running = False
+    #                     print(P2_WIN_MSG)
 
-                    if cols_win_o(board) and p2_letter.upper() == '0':
-                        running = False
-                        print(P2_WIN_MSG)
+    #                 if cols_win_o(board) and p2_letter.upper() == '0':
+    #                     running = False
+    #                     print(P2_WIN_MSG)
                         
-                    if diagnols_win_o(board) and p2_letter.upper() == '0':
-                        running = False            
-                        print(P2_WIN_MSG)
+    #                 if diagnols_win_o(board) and p2_letter.upper() == '0':
+    #                     running = False            
+    #                     print(P2_WIN_MSG)
 
-    
 
-            
 
                 
 
-    except ValueError:
-            print('Enter a valid number between 1 to 9')
+ 
 
 
 
