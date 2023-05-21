577936d1bd6c891f51efcc29767d2577424816ed
Feisal Ahmed <feisala132@gmail.com>
2023-05-21 20:48:13 +0100

    Adding functions to check for win or tie

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -24,12 +24,59 @@ def print_board(board):
 def player_input(board):
         try:
             inp = int(input("Enter a number 1-9: "))
+            if inp >= 1 and inp <= 9 and board[inp-1] == '-':
+                board[inp-1] = CurrentPlayer
+            else:
+                print("That position is already taken by other player")
         except ValueError:
              print("Enter a valid number from 1 to 9")
-        if inp >= 1 and inp <= 9 and board[inp-1] == '-':
-            board[inp-1] = CurrentPlayer
-        else:
-            print("That position is already taken by other player")
+     
+
+
+
+# Check for win or tie
+def check_row(board):
+    global Winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        Winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        Winner = board[1]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        Winner = board[6]
+        return True
+
+def check_column(board):
+    global Winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        Winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        Winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        Winner = board[2]
+        return True
+    
+def check_diagnols(board):
+    global Winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        Winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        Winner = board[2]
+        return True
+    
+def check_tie(board):
+    if '-' not in board:
+        print_board(board)
+        print("It is a tie!")
+        gameRunning = False
+    
+
+
+
 
 
 # def cols_win_x(board):
@@ -64,14 +111,14 @@ def player_input(board):
 
         
 
-while gameRunning:
+while gameRunning:#
+    print_board(board)
+    player_input(board)
     # allowed_nums = [1,2,3,4,5,6,7,8,9]
     # P1_WIN_MSG = 'Player 1 Wins!'
     # P2_WIN_MSG = 'Player 2 Wins!'
     # count =  0 # count variablle to check for '-' used for checking for draw
-    
-    print_board(board)
-    player_input(board)
+
     # Player one
    
 
